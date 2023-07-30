a8876fcdbe4d20c8da427daa484f4e3b71bfcfb9
Ian Chua <ianchuajl@hotmail.com>
2023-07-30 20:53:06 +1200

    added results of the users choice

--- mind-reader/main.js
+++ mind-reader/main.js
@@ -1,12 +1,37 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//   const form = document.getElementById('mindReaderForm');
+
+//   form.addEventListener('submit', (event) => {
+//     event.preventDefault(); // Prevent page reload
+
+//     const selectedNumber = document.getElementById('selectedNumber').value;
+
+//     // Store the selected number in Local Storage
+//     localStorage.setItem('selectedNumber', selectedNumber);
+
+//     // Redirect to the different HTML page
+//     window.location.href = 'result.html';
+//   });
+// });
+
 document.addEventListener('DOMContentLoaded', () => {
-  const form = document.getElementById('mindReaderForm')
+  const readMindButton = document.getElementById('readMindButton');
 
-  form.addEventListener('submit', (event) => {
-    event.preventDefault() // Prevent page reload
+  readMindButton.addEventListener('click', () => {
+    const selectedNumber = document.getElementById('selectedNumber').value;
 
-    const selectedNumber = document.getElementById('selectedNumber').value
-    
-    // Do something with the selectedNumber
-    console.log('User selected number:', selectedNumber)
-  })
-})
+    // Store the selected number in Local Storage
+    localStorage.setItem('selectedNumber', selectedNumber);
+
+    // Redirect to the different HTML page
+    window.location.href = 'result.html';
+  });
+});
+
+
+ // Retrieve the stored number from Local Storage
+ const storedNumber = localStorage.getItem('selectedNumber');
+
+ // Display the stored number on the page
+ const resultDiv = document.getElementById('result');
+ resultDiv.textContent = `${storedNumber}`;
