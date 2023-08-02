d61880c97403dcf95c7a07e71b452c280640dd94
Ian Chua <ianchuajl@hotmail.com>
2023-08-02 19:51:20 +1200

    added an array for the button for now

---
+++ button-pusher/main.js
@@ -0,0 +1,9 @@
+const messages = [
+  "Please don't push the button.",
+  'Are you for real....',
+  'Wow really',
+  "Don't you have anything else to do?",
+  'You know  that time is money right?',
+  "Ok, you're gonna just do it anyway",
+  "You can't be serious...",
+]
--- mind-reader/main.js
+++ mind-reader/main.js
@@ -1,19 +1,4 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//   const form = document.getElementById('mindReaderForm');
-
-//   form.addEventListener('submit', (event) => {
-//     event.preventDefault(); // Prevent page reload
-
-//     const selectedNumber = document.getElementById('selectedNumber').value;
-
-//     // Store the selected number in Local Storage
-//     localStorage.setItem('selectedNumber', selectedNumber);
-
-//     // Redirect to the different HTML page
-//     window.location.href = 'result.html';
-//   });
-// });
-
+// Obtaining and storing user input
 document.addEventListener('DOMContentLoaded', () => {
   const readMindButton = document.getElementById('readMindButton');
 
@@ -28,10 +13,27 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-
  // Retrieve the stored number from Local Storage
  const storedNumber = localStorage.getItem('selectedNumber');
 
  // Display the stored number on the page
  const resultDiv = document.getElementById('result');
  resultDiv.textContent = `${storedNumber}`;
+
+
+// MIGHT BE USEFUL LATER ON
+ // document.addEventListener('DOMContentLoaded', () => {
+//   const form = document.getElementById('mindReaderForm');
+
+//   form.addEventListener('submit', (event) => {
+//     event.preventDefault(); // Prevent page reload
+
+//     const selectedNumber = document.getElementById('selectedNumber').value;
+
+//     // Store the selected number in Local Storage
+//     localStorage.setItem('selectedNumber', selectedNumber);
+
+//     // Redirect to the different HTML page
+//     window.location.href = 'result.html';
+//   });
+// });
