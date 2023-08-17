f889ac917f830a252a92fcfde42071213284f247
Ian Chua <ianchuajl@hotmail.com>
2023-08-17 17:57:15 +1200

    message now changes when button is pressed

--- button-pusher/main.js
+++ button-pusher/main.js
@@ -1,3 +1,4 @@
+// Messages to be displayed
 const messages = [
   "Please don't push the button.",
   'Are you for real....',
@@ -7,3 +8,14 @@ const messages = [
   "Ok, you're gonna just do it anyway",
   "You can't be serious...",
 ]
+
+// getting those elements by ID
+const changeText = document.getElementById('changeText');
+const pushButton = document.getElementById('pushableButton');
+
+let currentMessageIndex = 0;
+
+pushButton.addEventListener('click', () => {
+  changeText.textContent = messages[currentMessageIndex];
+  currentMessageIndex = (currentMessageIndex + 1) % messages.length;
+});
