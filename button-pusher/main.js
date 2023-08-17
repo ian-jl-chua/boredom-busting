49477bd9c56326a27dd0003b5a7165a7fc8ae030
Ian Chua <ianchuajl@hotmail.com>
2023-08-17 18:16:27 +1200

    refactor the click function

--- button-pusher/main.js
+++ button-pusher/main.js
@@ -1,21 +1,27 @@
 // Messages to be displayed
 const messages = [
-  "Please don't push the button.",
-  'Are you for real....',
+  "Please don't touch the button.",
+  'Are you for real...',
   'Wow really',
   "Don't you have anything else to do?",
-  'You know  that time is money right?',
-  "Ok, you're gonna just do it anyway",
+  'You know that time is money right?',
+  'You are throwing away money by pressing this.',
+  "Ok, you're gonna just do it anyway...",
   "You can't be serious...",
+  '*sigh*',
 ]
 
+let currentMessageIndex = 0
+
+function buttonClick() {
+  changeText.textContent = messages[currentMessageIndex] //changing text context of the paragraph element
+
+  currentMessageIndex = (currentMessageIndex + 1) % messages.length
+}
+
 // getting those elements by ID
-const changeText = document.getElementById('changeText');
-const pushButton = document.getElementById('pushableButton');
+const changeText = document.getElementById('changeText')
+const pushButton = document.getElementById('pushableButton')
 
-let currentMessageIndex = 0;
-
-pushButton.addEventListener('click', () => {
-  changeText.textContent = messages[currentMessageIndex];
-  currentMessageIndex = (currentMessageIndex + 1) % messages.length;
-});
+// Event listener for the click
+pushButton.addEventListener('click', buttonClick)
