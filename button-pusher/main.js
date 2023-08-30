9a13c14bd449e62b4bea10d79c0676885de8c50e
Ian Chua <ianchuajl@hotmail.com>
2023-08-30 13:09:26 +1200

    color div and seisure function WORKS

--- button-pusher/main.js
+++ button-pusher/main.js
@@ -68,68 +68,122 @@ function buttonMove() {
 function resetButtonPosition() {
   pushButton.style.gridArea = '4/5/5/6' // Reset button position
   subsequentClickCount = 0 // Reset the count for buttonMove
+  const secondMsg = document.querySelector('.messageTwo')
+
+  secondMsg.hidden = true
 }
 // Logic for 4th action
 function colorCubes() {
-  const colorDiv = document.createElement('div')
+  changeText.textContent = "It's SEIZURE TIME!!!!!!!!!!!!!"
 
-  colorDiv.classList.add('colorChange')
+  const seizureDiv = document.createElement('div')
 
-  explode.parentElement.insertBefore(colorDiv, explode)
-}
+  seizureDiv.classList.add('changeColor')
 
-// Logic for 5th action
-function kaboom() {
-  if (!hasKaboomed) {
-    secondChangeText.textContent = ''
-    changeText.textContent = 'OMG what have you done...'
-    explode.hidden = false
+  explode.parentElement.insertBefore(seizureDiv, explode)
 
-    const excludeGridAreas = [
-      [1, 5, 2, 6], //title
-      [6, 4, 7, 7],
-      [4, 5, 5, 6],
-    ] // grids to be excluded from explosion
-
-    // setInterval handles the explosion effect to make it random and more natural
-    setInterval(() => {
-      let randomRowStart, randomRowEnd, randomColStart, randomColEnd
-
-      do {
-        randomRowStart = Math.floor(Math.random() * 7) + 1
-        randomRowEnd = randomRowStart + 1
-        randomColStart = Math.floor(Math.random() * 9) + 1
-        randomColEnd = randomColStart + 1
-      } while (
-        excludeGridAreas.some(
-          (area) => area[0] === randomRowStart && area[1] === randomColStart
-        )
-      )
-
-      explode.style.gridArea = `${randomRowStart}/${randomColStart}/${randomRowEnd}/${randomColEnd}`
-    }, 1500)
-
-    hasKaboomed = true
+  // Generate a random RGB color
+  function generateRandomColor() {
+    const r = Math.floor(Math.random() * 256)
+    const g = Math.floor(Math.random() * 256)
+    const b = Math.floor(Math.random() * 256)
+    return `rgb(${r},${g},${b})`
   }
+
+  const colors = [
+    generateRandomColor(),
+    generateRandomColor(),
+    generateRandomColor(),
+    generateRandomColor(),
+    generateRandomColor(),
+    generateRandomColor(),
+    generateRandomColor(),
+    generateRandomColor(),
+    generateRandomColor(),
+    generateRandomColor(),
+    generateRandomColor(),
+  ]
+
+  colors.forEach((color, index) => {
+    seizureDiv.style.setProperty(`--color${index}`, color)
+  })
 }
-// Reset 5th action
-function resetKaboom() {
-  explode.hidden = true
+// Reset 4th action
+function resetColorCubes() {
   changeText.textContent = ''
+  secondChangeText.textContent = ''
+  const elementsToRemove = document.querySelectorAll('.changeColor')
+
+  // Loop through the selected elements and remove them
+  elementsToRemove.forEach((element) => {
+    element.remove()
+  })
+  
+  const secondMsg = document.querySelector('.messageTwo')
+  
+  secondMsg.hidden = false
+
+  subsequentClickCount = 0
 }
 
+// // Logic for 5th action
+// function kaboom() {
+//   if (!hasKaboomed) {
+//     secondChangeText.textContent = ''
+//     changeText.textContent = 'OMG what have you done...'
+//     explode.hidden = false
+
+//     const excludeGridAreas = [
+//       [1, 5, 2, 6], //title
+//       [6, 4, 7, 7],
+//       [4, 5, 5, 6],
+//     ] // grids to be excluded from explosion
+
+//     // setInterval handles the explosion effect to make it random and more natural
+//     setInterval(() => {
+//       let randomRowStart, randomRowEnd, randomColStart, randomColEnd
+
+//       do {
+//         randomRowStart = Math.floor(Math.random() * 7) + 1
+//         randomRowEnd = randomRowStart + 1
+//         randomColStart = Math.floor(Math.random() * 9) + 1
+//         randomColEnd = randomColStart + 1
+//       } while (
+//         excludeGridAreas.some(
+//           (area) => area[0] === randomRowStart && area[1] === randomColStart
+//         )
+//       )
+
+//       explode.style.gridArea = `${randomRowStart}/${randomColStart}/${randomRowEnd}/${randomColEnd}`
+//     }, 1500)
+
+//     hasKaboomed = true
+//   }
+// }
+// // Reset 5th action
+// function resetKaboom() {
+//   explode.hidden = true
+//   changeText.textContent = ''
+// }
+
+// if (subsequentClickCount >= 10) {
+//     resetKaboom() //resets kaboom once user presses button 10 times
+//   } else {
+//     kaboom()
+//     subsequentClickCount++
+//     console.log(subsequentClickCount)
+//   }
 // ACTUAL BUTTON ACTIONS
 function buttonClick() {
-  // if (subsequentClickCount >= 10) {
-  //     resetKaboom() //resets kaboom once user presses button 10 times
-  //   } else {
-  //     kaboom()
-  //     subsequentClickCount++
-  //     console.log(subsequentClickCount)
-  //   }
-
   if (mainClickCount >= 26) {
-    colorCubes()
+    if (subsequentClickCount >= 58) {
+      resetColorCubes()
+    } else {
+      colorCubes()
+      subsequentClickCount++
+    }
+    // console.log('sub', subsequentClickCount)
+    // console.log('mainclicks', mainClickCount)
   } else if (mainClickCount >= 15) {
     if (subsequentClickCount >= 10) {
       resetButtonPosition()
@@ -137,14 +191,19 @@ function buttonClick() {
       buttonMove() // does this function 10 times
       subsequentClickCount++
     }
-    console.log(mainClickCount)
+    // console.log(mainClickCount)
+    // console.log('sub', subsequentClickCount)
   } else if (mainClickCount >= 10) {
     secondMessages()
+    // console.log(mainClickCount)
+    // console.log('sub', subsequentClickCount)
   } else {
     firstMessages()
+    // console.log(mainClickCount)
+    // console.log('sub', subsequentClickCount)
   }
 
-  mainClickCount = (mainClickCount + 1) % 37
+  mainClickCount = (mainClickCount + 1) % 85
 }
 
 // getting those elements by ID
